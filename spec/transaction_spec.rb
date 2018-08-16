68863c4df8bb9480018e398a9502a470ed091b78
Bibi Collins <bibicollins94@googlemail.com>
2018-08-16 17:11:10 +0100

    Rubocop fixes

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,7 +1,7 @@
 require 'transaction'
 require 'spec_helper'
 describe Transaction do
-  let(:transaction) { Transaction.new("#{test_time}", 'debit', 'credit', 'balance') }
+  let(:transaction) { Transaction.new(test_time.to_s, 'debit', 'credit', 'balance') }
   let(:time) { test_time }
   it 'stores the date of any transaction' do
     expect(transaction.instance_variable_get('@date')).to eq time
